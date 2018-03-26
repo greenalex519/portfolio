d81409b8264b8530e8a6867da3401b17e3f2b6be
greenalex519 <32032234+greenalex519@users.noreply.github.com>
2018-03-26 17:08:55 +1100

    added test files

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     $("navtitle").hover(function() {
+//         $(this).siblings("navbar").addClass("showmenu"); //for 3 seconds etc
+//     }, function() {                                         //get rid of?
+//         $(this).siblings("navbar").removeClass("showmenu");  //get rid off?
+//     });
+// });
